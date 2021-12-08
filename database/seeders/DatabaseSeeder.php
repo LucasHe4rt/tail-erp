e5e283b99ce4436eb4d00883185052da6612fcb4
LucasHe4rt <lucassilvabittencourt@protonmail.ch>
2021-12-07 21:40:29 -0300

    Added product category and user factory to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product\ProductCategory;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        User::factory()->count(100)->create();
+        ProductCategory::factory()->count(20)->create();
         // $this->call('UsersTableSeeder');
     }
 }
