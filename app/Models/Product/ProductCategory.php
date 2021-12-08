49fd305a76969654442ff689fd9d472ab461147a
LucasHe4rt <lucassilvabittencourt@protonmail.ch>
2021-12-07 21:39:57 -0300

    Added Product category factory

--- app/Models/Product/ProductCategory.php
+++ app/Models/Product/ProductCategory.php
@@ -2,10 +2,13 @@
 
 namespace App\Models\Product;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductCategory extends Model
 {
+    use HasFactory;
+    
     protected $fillable = [
         'name', 'description'
     ];
