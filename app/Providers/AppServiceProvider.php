5b4428ee13d699a7bd54791c8554b2c0c7b57e90
LucasHe4rt <lucassilvabittencourt@protonmail.ch>
2021-12-07 21:37:58 -0300

    Added Faker

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,11 +5,13 @@ namespace App\Providers;
 use App\Repositories\Eloquent\BaseRepository;
 use App\Repositories\Eloquent\Product\ProductCategoryRepository;
 use App\Repositories\Eloquent\Product\ProductRepository;
-use App\Repositories\EloquentRepositoryInterface;
 use App\Repositories\Eloquent\UserRepository;
+use App\Repositories\EloquentRepositoryInterface;
 use App\Repositories\ProductCategoryRepositoryInterface;
 use App\Repositories\ProductRepositoryInterface;
 use App\Repositories\UserRepositoryInterface;
+use Faker\Factory;
+use Faker\Generator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +27,9 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
         $this->app->bind(ProductCategoryRepositoryInterface::class, ProductCategoryRepository::class);
         $this->app->bind(ProductRepositoryInterface::class, ProductRepository::class);
+
+        $this->app->singleton(Generator::class, function () {
+            return Factory::create('pt_BR');
+        });
     }
 }
